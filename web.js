3392e1d1b69deee466569cc4e6ac8a04b74279ff
Rachel Leksana <rtjandra@andrew.cmu.edu>
2014-05-20 11:09:05 -0400

    html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var querystring = require("querystring");
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var path = require("path");
 var app = express();
-var http = require('http');
-var fs = require('fs');
 var collections = ["CFA", "Baker", "Wean", "Cyert", "Hunt"];
 var db = require("mongojs").connect(process.env.MONGOHQ_URL, collections);
 
@@ -12,18 +15,7 @@ app.use(logfmt.requestLogger());
 app.get('/', function(req, res) {res.send("hello world"); });
 
 app.get('/CFA', function(req, res) {
-	fs.readFile('CFA.html', function(err, html){
-		if(err){
-			console.log("error");
-		}
-		else {
-			http.createServer(function(htmlReq, htmlRes){
-				htmlRes.writeHeader(200, {"Content-Type": "text/html"});
-				htmlRes.write(html);
-				htmlRes.end();
-			});
-		}
-	});
+	serveStaticHTML(res, "CFA.html");
 });
 
 var port = Number(process.env.PORT || 5000);
