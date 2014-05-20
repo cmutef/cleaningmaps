e3e47715892de8ecc2330c74d6232727fd9a09c8
Rachel Leksana <rtjandra@andrew.cmu.edu>
2014-05-20 10:08:10 -0400

    adding mongo

--- web.js
+++ web.js
@@ -2,11 +2,37 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var databaseUrl = "clusters";
+var collections = ["CFA", "Baker", "Wean", "Cyert", "Hunt"];
+var db = require("mongojs").connect(databaseUrl, collections);
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+
+for(var i = 1; i < 21; i++){
+  var n = "";
+  if(i < 10) n = "0";
+  var d = new Date();
+  db.CFA.save({name:"CFA318-"+n+i, andrew:"test", date:d}, function(err, save){
+  	if(err)
+  		console.log("error");
+  	else
+  		console.log("saved");
+  });
+}
+
+db.CFA.find(function(err, comp){
+  	if(err) 
+  	    console.log("error");
+  	else
+  		res.send(comp);
+  });
+
+});
+
+app.get('/CFA', function(req, res) {
+	res.send([{name:'CFA1'}, {name:'CFA2'}]);
 });
 
 var port = Number(process.env.PORT || 5000);
