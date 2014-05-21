e8a42cf30935e9d9b88f78159f67705e6242b453
Rachel Leksana <rtjandra@andrew.cmu.edu>
2014-05-21 10:25:38 -0400

    connect

--- web.js
+++ web.js
@@ -6,9 +6,11 @@ var fs = require("fs");
 var app = express();
 var collections = ["CFA", "Baker", "Wean", "Cyert", "Hunt"];
 var db = require("mongojs").connect(process.env.MONGOHQ_URL, collections);
+var connect = require("connect");
+var CFAhtml = "<html><head></head><body></body></html>"
 
-var CFAhtml = "<html><head></head><body>hello world</body></html>"
 
+connect().use(connect.static(__dirname));
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {res.send("hello world"); });
