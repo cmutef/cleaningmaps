4829a2ce0adc41d3a892d1553e8a096352adb586
Rachel Leksana <rtjandra@andrew.cmu.edu>
2014-05-21 20:57:56 -0400

    bodyparser

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
-var bodyParser = require("body-parser");
 var fs = require("fs");
 var app = express();
 var collections = ["CFA", "Baker", "Wean", "Cyert", "Hunt"];
 var db = require("mongojs").connect(process.env.MONGOHQ_URL, collections);
 var CFAhtml = fs.readFileSync("CFA.html", 'utf8');
 
-app.use(bodyParser());
+app.use(express.json());
 
 app.get('/', function(req, res) {res.send("hello world"); });
 
