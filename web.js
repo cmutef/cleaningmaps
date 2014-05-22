3ad46ce0a5a42f8c73cd0ce0e5f51a593af23974
Rachel Leksana <rtjandra@andrew.cmu.edu>
2014-05-22 13:33:12 -0400

    baker

--- web.js
+++ web.js
@@ -20,14 +20,6 @@ app.get('/CFA', function(req, res) {
 
 app.get('/BH', function(req, res) {
 	var d = new Date();
-	
-db.CFA.save({
-	name:"BH140C-10",
-	andrew:"test",
-	date:d
-});
-
-
     res.send(""+BHhtml);
 });
 
