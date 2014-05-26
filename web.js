e3a1af12c93a3168a22b89a3f1b5521e4210d2d8
Rachel Leksana <rtjandra@andrew.cmu.edu>
2014-05-26 15:35:13 -0400

    andrew

--- web.js
+++ web.js
@@ -16,11 +16,8 @@ var Cyerthtml = fs.readFileSync("Cyert.html", 'utf8');
 app.use(bodyParser());
 
 app.get('/', function(req, res) {
-	var d = new Date();
-	db.CFA.save({
-		name:"BH140C-19",
-		andrew:"test",
-		date:d
+	db.CFA.find(function(err, docs){
+		res.send(docs);
 	});
     res.send(""+indexhtml); 
 });
